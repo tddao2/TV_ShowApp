11b315a94bc4ce1ac430e094c0370b630be17650
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-02 12:14:34 -0500

    Adding the first TV show

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ form.addEventListener('submit', async (e) => {
     const res = await axios.get(
         `https://api.tvmaze.com/search/shows?q=${searchTerm}`
     );
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img);
 });
