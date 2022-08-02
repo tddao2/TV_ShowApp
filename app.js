a10706cddc37d5bf1a5e038e3a4f7c3ef322b7b2
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-02 12:28:22 -0500

    Configuring params to get multiple search params

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(
-        `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-    );
+    // const res = await axios.get(
+    //     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+    // );
+    const config = { params: { q: searchTerm, isFunny: 'Cristian' } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
     makeImages(res.data);
+    form.elements.query.value;
 });
 const makeImages = (shows) => {
     for (let result of shows) {
